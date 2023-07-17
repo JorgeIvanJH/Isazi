2683115c105457e5ec7e0a4c05c1851cfa0d8b62
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2023-07-17 01:58:44 -0500

    function documentation and model units correction

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import pandas as pd
 energy_model = joblib.load('models/energy_model.pkl')
 poly_features_model = joblib.load('models/poly_features_model.pkl')
 
-def get_energy_cost(gradient:np.array,poly_features_model,energy_model)->np.array:
+def get_energy_cost(gradient:np.array,poly_features_model,energy_model,weight:float=80,vel:float=50)->np.array:
     """
     Calculates the energy cost based on the gradient using a polynomial regression model.
 
@@ -15,6 +15,8 @@ def get_energy_cost(gradient:np.array,poly_features_model,energy_model)->np.arra
         gradient (np.array): Input gradient values.
         poly_features_model: Pre-trained polynomial features object.
         energy_model: Pre-trained regression model.
+        weight [Kg]: Weight of a person
+        vel [m/min]: Velocity mantained from a person
 
     Returns:
         np.array: Predicted energy cost values, shape (n_samples,).
@@ -22,9 +24,10 @@ def get_energy_cost(gradient:np.array,poly_features_model,energy_model)->np.arra
     gradient = gradient.reshape(-1,1)
     x_poly = poly_features_model.transform(gradient)
     y_pred = energy_model.predict(x_poly)
-    return y_pred
+    energy = y_pred*weight/vel
+    return energy
 
-def slope_computing(a_h:float,b_h:float,horiz_diff:float = 10):
+def slope_computing(a_h:float,b_h:float,horiz_diff:float = 10)->np.float64:
     """
     Calculates the slope between 2 different heights and a fixed horizontal displacement.
 
@@ -39,9 +42,17 @@ def slope_computing(a_h:float,b_h:float,horiz_diff:float = 10):
     height_diff = b_h-a_h
     return np.arctan(height_diff/horiz_diff)
 
-def build_node_graph(df_alt_map,node_graph,min_slope:float=-0.46,max_slope:float=0.46):
+def build_node_graph(df_alt_map,node_graph,min_slope:float=-0.46,max_slope:float=0.46)->None:
     """
-    Skips node connections if slope excedes the range of data provided and on which the slope model is confident
+    Adds connections to a a node graph for the data presented
+    Skips node connections if slope excedes the range of data provided; on which the slope model is confident
+    
+    Args:
+        df_alt_map (np.dataframe): dataframe containing maps height
+        node_graph: Pre built empty node graph
+        min_slope (np.float): minimum valid slope
+        max_slope (np.float): maximum valid slope
+
     """
 
     for i,row in df_alt_map.iterrows():
@@ -94,7 +105,17 @@ def build_node_graph(df_alt_map,node_graph,min_slope:float=-0.46,max_slope:float
                     node_graph.add_edge(f"({i},{j})",f"({i},{j-1})",weight=energy_cost)
             except: pass
 
-def compute_route(node_graph,starting_point:tuple=(0,0),end_point:tuple=(200,559)):
+def compute_route(node_graph,starting_point:tuple=(0,0),end_point:tuple=(200,559))->pd.DataFrame:
+    """
+    Calulates the trail path which minimixes the energy excerted
+    
+    Args:
+        node_graph: node graph with the needed connections
+        starting_point (tuple): wanted starting point (x,y)
+        end_point (tuple):  wanted ending point (x,y)
+    Returns:
+        pd.DataFrame: slope between heights in radians
+    """
     starting_point = starting_point[::-1]
     end_point = end_point[::-1]
     starting_point = f"({starting_point[0]},{starting_point[1]})"
